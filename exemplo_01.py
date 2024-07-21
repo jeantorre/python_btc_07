f8403129b79028036b130c4897c58db0608c97f2
jeantorre <jean.torre21@gmail.com>
2024-07-21 11:49:54 -0300

    exemplo aula bootcamp

---
+++ exemplo_01.py
@@ -0,0 +1,10 @@
+def somar_valores(valor_1: float, valor_2: float) -> float:
+    # doc string
+    '''
+    função simples para soma soma de 2 valores do tipo float
+    '''
+    resultado_soma = valor_1 + valor_2
+
+    return print(resultado_soma)
+
+somar_valores(5.4, 10.4)
